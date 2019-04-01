9e1bf6dd7100b6b0bc123a55ab49207fa8ea7773
yaoyili <1164881099@qq.com>
2019-04-01 18:27:20 +0800

    开始做导航功能：framgent

--- app/src/main/java/com/example/belief/ui/MainActivity.java
+++ app/src/main/java/com/example/belief/ui/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.belief.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.belief.R;
+import com.example.belief.utils.BNVEffect;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.buttom_nav_view)
+    public BottomNavigationView buttomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        BNVEffect.disableShiftMode(buttomNav);
     }
+
 }
---
+++ app/src/main/java/com/example/belief/utils/BNVEffect.java
@@ -0,0 +1,33 @@
+package com.example.belief.utils;
+
+import android.annotation.SuppressLint;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+public class BNVEffect {
+    //去除底部导航的滑动效果
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i =0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVEffect","Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVEffect","Unable to change value of shift mode", e);
+        }
+    }
+
+}
