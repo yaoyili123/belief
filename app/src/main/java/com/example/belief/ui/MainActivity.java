7c848d5e2ea1ae5cb9e60c1b2e3ba646b93b6b3a
yaoyili <1164881099@qq.com>
2019-04-02 18:31:48 +0800

    去掉了MainFragments，如果只有一个活动的话，底部栏和ToolBar是不能去掉的

--- app/src/main/java/com/example/belief/ui/MainActivity.java
+++ app/src/main/java/com/example/belief/ui/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.belief.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.belief.R;
 import com.example.belief.ui.base.BaseActivity;
+import com.example.belief.utils.BNVEffect;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.yokeyword.fragmentation.SupportFragment;
 import me.yokeyword.fragmentation.anim.DefaultHorizontalAnimator;
 import me.yokeyword.fragmentation.anim.FragmentAnimator;
 
@@ -22,24 +27,47 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.top_toolbar)
     public Toolbar mTitle;
 
-    public void setToolbarTitle(int res) {
-        mTitle.setTitle(res);
-    }
+    @BindView(R.id.buttom_nav_view)
+    public BottomNavigationView bnv;
+
+    private MainActivity mActivity;
+
+    private MenuItem mi;
+
+    private SupportFragment[] mFragments = new SupportFragment[4];
+
+    private int prePos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.frag_main);
         ButterKnife.bind(this);
-
-        //设置Activity默认的ACTIONbAR，不需要在布局中再加上toolbar控件
         setSupportActionBar(mTitle);
-        assert mTitle != null;
+        BNVEffect.disableShiftMode(bnv);
+        bnv.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        //activity初始加载HomeFragment
-        if (findFragment(MainFragment.class) == null)
-            //左侧参数是加载位置
-            loadRootFragment(R.id.main_act, MainFragment.newInstance());
+        SupportFragment firstFragment = findFragment(SportMainFragment.class);
+        if (firstFragment == null) {
+            mFragments[0] = SportMainFragment.newInstance("运动");
+            mFragments[1] = TestFragment.newInstance("1");
+            mFragments[2] = TestFragment.newInstance("2");
+            mFragments[3] = UserMainFragment.newInstance("用户");
+
+            loadMultipleRootFragment(R.id.blank_frag, 0,
+                    mFragments[0],
+                    mFragments[1],
+                    mFragments[2],
+                    mFragments[3]);
+        } else {
+            // 这里库已经做了Fragment恢复,所有不需要额外的处理了, 不会出现重叠问题
+
+            // 这里我们需要拿到mFragments的引用
+            mFragments[0] = firstFragment;
+            mFragments[1] = findFragment(TestFragment.class);
+            mFragments[2] = findFragment(TestFragment.class);
+            mFragments[3] = findFragment(UserMainFragment.class);
+        }
     }
 
     //监听所有fragments的callbacks，实现FragmentManager.FragmentLifecycleCallbacks中的callbacks
@@ -61,4 +89,42 @@ public class MainActivity extends BaseActivity {
         // 设置横向(和安卓4.x动画相同)
         return new DefaultHorizontalAnimator();
     }
+
+    //底部导航选中callback
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            mi = item;
+            switch (item.getItemId()) {
+                case R.id.bn_sport: {
+                    mTitle.setTitle(R.string.top_title_sport);
+                    showHideFragment(mFragments[0], mFragments[prePos]);
+                    prePos = 0;
+                    return true;
+                }
+                case R.id.bn_recipe: {
+                    mTitle.setTitle(R.string.top_title_recipe);
+                    showHideFragment(mFragments[1], mFragments[prePos]);
+                    prePos = 1;
+                    return true;
+                }
+                case R.id.bn_comm: {
+                    mTitle.setTitle(R.string.top_title_comm);
+                    showHideFragment(mFragments[2], mFragments[prePos]);
+                    prePos = 2;
+                    return true;
+                }
+                case R.id.bn_user:{
+                    mTitle.setTitle(R.string.top_title_user);
+                    showHideFragment(mFragments[3], mFragments[prePos]);
+                    prePos = 3;
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
 }
