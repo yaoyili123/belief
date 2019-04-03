6c3971945ae878fc8db53c2512228b14f9fd5ade
yaoyili <1164881099@qq.com>
2019-04-03 18:51:33 +0800

    整合了资源2

---
+++ app/src/main/java/com/example/belief/ui/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.belief.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.belief.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchlayout);
+    }
+}
